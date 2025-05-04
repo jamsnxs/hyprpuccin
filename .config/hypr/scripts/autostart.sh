9b799c5076748bf99bc6517fc4d30b7101e719c2
jamsnxs <jailsonhh72c@gmail.com>
2025-05-04 03:34:43 -0300

    Update

--- .config/hypr/scripts/autostart.sh
+++ .config/hypr/scripts/autostart.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 swww-daemon &
-# Tenta restaurar o último wallpaper usado
-sleep 0.5 && swww restore || swww img ~/Pictures/hyprland/png.png --transition-step 80 --transition-fps 80 --transition-type center --transition-duration 1
+sleep 0.5 && swww restore
